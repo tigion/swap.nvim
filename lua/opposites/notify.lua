16ef585b99baace2f77c85f96cb3f35cc483e09f
Christoph Zirkelbach <christoph@tigion.de>
2025-06-26 15:30:33 +0200

    feat(notify): add escaping of special characters (wip)

--- lua/opposites/notify.lua
+++ lua/opposites/notify.lua
@@ -1,6 +1,14 @@
 ---@class opposites.Notify
 local M = {}
 
+---Escapes special characters in a string.
+---@param escaped_str string
+---@return string
+local function escape_special_chars(escaped_str)
+  escaped_str = string.gsub(escaped_str, '%[', '\\[') -- `[` -> `\[`
+  return escaped_str
+end
+
 ---Displays a notification to the user.
 ---@param message string
 ---@param level? number -- vim.log.levels
@@ -9,7 +17,7 @@ local function notify(message, level, cursor)
   level = level or vim.log.levels.INFO
   if cursor ~= nil then
     local row_col_str = '[' .. cursor.row .. ':' .. cursor.col + 1 .. ']'
-    message = row_col_str .. ' ' .. message
+    message = row_col_str .. ' ' .. escape_special_chars(message)
   end
   vim.notify(message, level, { title = 'Opposites' })
 end
